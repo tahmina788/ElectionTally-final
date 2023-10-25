20fcdd48bf92b71b9e50dc392512e98a5b463cd3
tahmina788 <54203532+tahmina788@users.noreply.github.com>
2023-10-25 19:54:23 +0600

    Update app.js

--- backendAdmin/app.js
+++ backendAdmin/app.js
@@ -16,7 +16,7 @@ console.log('cookieParser')
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 dotenv.config({ path: './config.env' });
-//dotenv.config();
+
 
 const hostname = process.env.HOST;
 
@@ -24,20 +24,7 @@ console.log(hostname)
 
 mongoose.set('strictQuery', false);
 
- //const hostname = "localhost";
- //const hostname = "192.168.200.249";
- //const hostname = "192.168.0.102";
-
 // Initialize DB
-{/* 
-mongoose
- .connect('mongodb+srv://tahminabithe47:01757112809A@electiontallywithlogin.1x4u0rl.mongodb.net/',{
-     useNewUrlParser: true,
-     useUnifiedTopology: true,
- })
- .then(() => console.log('connection successful'))
- .catch(err => console.log(`no connection`))
- */}
  
  mongoose
 .connect('mongodb://mongo:27017/registrationlogin',{
@@ -46,35 +33,6 @@ mongoose
 })
 .then(() => console.log('connection successful'))
 .catch(err => console.log(`no connection`))
-
-{/* 
-mongoose
-  .connect('mongodb://mongo:27017/registrationlogin', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('Connection successful');
-  })
-  .catch((err) => {
-    console.error('Connection failed:', err);
-  });
-*/}
-
-  // database connection with mongoose
-  {/* 
- mongoose
- .connect('mongodb://localhost/registrationlogin',{
-     useNewUrlParser: true,
-     useUnifiedTopology: true,
- })
- .then(() => {
-  console.log('Connection successful');
-})
-.catch((err) => {
-  console.error('Connection failed:', err);
-});
-*/}
 
 
 const User = require('./model/userSchema');
@@ -89,51 +47,10 @@ app.use(cors({
   credentials: true
  }));
 
- 
- 
-{/* 
-const allowedOrigins = ['http://192.168.200.249:3000'];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true, // Allow credentials (cookies, etc.) to be sent with the request
-};
-
-app.use(cors(corsOptions));
-*/}
-
-// we link the router files to make our route easy
-
-
-//app.use(cors()); // Enable CORS for all routes
-
-// Enable CORS
-
-
-
-//app.use(cors({ credentials: true }));
-
-
-
- 
-
 
 //*******************************************************************//
 
-{/*}
-app.get('/', function (req, res) {
-  // Cookies that have not been signed
-  console.log('Cookies: ', req.cookies)
 
-  // Cookies that have been signed
-  console.log('Signed Cookies: ', req.signedCookies)
-})*/}
 
 
 app.use(
