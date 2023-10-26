224b4e48f644ec74059b8cb1c2c35b2c3e48f8dc
tahmina00 <tahminacs@gmail.com>
2023-10-26 19:03:05 +0600

    ElectionTally-final main branch commit

--- frontendAdmin/src/components/Addelectionname.js
+++ frontendAdmin/src/components/Addelectionname.js
@@ -15,47 +15,27 @@ const { REACT_APP_HOST } = process.env;
 const hostname = `${REACT_APP_HOST}`;
 
 const Addelectionname = () => {
-
 	const history = useHistory();
-
 	const [statusvalue, setStatusvalue] = useState(1);
-
 	const [electionNameData, setelectionNameData] = useState({
 		banglaelectionname: "", englishelectionname: "", statusfordisplay: "", templatetype: ""
 	});
-
-
 	// read election value from database
-
 	const [electionData, setElectionData] = useState([]);
-
 	let name, value;
-
-
-
 	// we are storing data in states
-
 	const handleInputs = (e) => {
 		console.log(e);
 		name = e.target.name;
 		value = e.target.value;
 		setelectionNameData({ ...electionNameData, [name]: value });
 	}
-
-
-
 	// send the data to backend
-
 	const electionNameForm = async (e) => {
 		e.preventDefault();
-
 		const { banglaelectionname, englishelectionname, statusfordisplay, templatetype } = electionNameData;
-
 		console.log('election post data');
-
 		const formattedElectionName = templatetype.toLowerCase().replace(/\s+/g, "-");
-
-
 		try {
 			const response = await fetch(`http://${hostname}:${port}/addelectionname`, {
 				method: 'POST',
@@ -69,31 +49,20 @@ const Addelectionname = () => {
 					templatetype: formattedElectionName
 				})
 			});
-
-			console.log('****************************************************************************')
 			const data = await response.json();
-
 			if (response.status === 201) {
-				//console.log(data.message); // "value inserted successfully"
 				window.alert("data inserted successfully");
 				console.log("data inserted successfully");
 				setelectionNameData({ ...electionNameData, banglaelectionname: "", englishelectionname: "", templatetype: "" })
 			} else {
-				//console.log("Error:", data.message);
 				window.alert("Invalid data");
 				console.log("Invalid data");
 			}
-
-
 		} catch (error) {
 			console.error(error);
 		}
-
 	};
-
-
 	// we are retrive data from db
-
 	const getElectionValue = async () => {
 		try {
 			const res = await fetch(`http://${hostname}:${port}/getelectiondata`, {
@@ -104,7 +73,6 @@ const Addelectionname = () => {
 				},
 				credentials: "include"
 			})
-
 			const data = await res.json();
 			console.log('Election data');
 			console.log(data);
@@ -121,79 +89,63 @@ const Addelectionname = () => {
 	useEffect(() => {
 		getElectionValue();
 	}, []);
-
-
-	return (
-		<>
-			<div className="start_form">
-				<div className="container">
-					<div className="row">
-						<div className="col-lg-10 offset-lg-l">
-							<div className="contact_form_container py-5">
-								<div className="contact_form_title">
-									<h2>Add Election Name</h2>
-								</div>
-								<br />
-
-								<form method="POST" id="contact_form" onSubmit={electionNameForm}>
-
-									<>
-										{/* <div className="form_input">
-         <label>Election id (serial id)</label>
-         <input type="text" onChange={handleInputs} value={electionNameData.electionid} name="electionid" id="electionid" placeholder="Enter your Electionid ( serial id)" />
-	  </div>*/}
-										<div className="form_input">
-											<label>Election name in Bangla</label>
-											<input type="text" onChange={handleInputs} value={electionNameData.banglaelectionname} name="banglaelectionname" id="banglaelectionname" placeholder="Enter your banglaelectionname" />
-										</div>
-										<div className="form_input">
-											<label>Election name in English</label>
-											<input type="text" onChange={handleInputs} value={electionNameData.englishelectionname} name="englishelectionname" id="englishelectionname" placeholder="Enter your englishelectionname" />
-										</div>
-										<div className="form_input">
-											<label>Template Type</label>
-											<input type="text" onChange={handleInputs} value={electionNameData.templatetype} name="templatetype" id="templatetype" placeholder="Enter your templatetype" />
-										</div>
-										<input type="hidden" name="statusfordisplay" defaultValue={statusvalue} />
-
-
-										<br />
-
-										<button type="submit">Submit</button>
-
-
-									</>
-
-								</form>
-							</div>
-						</div>
+return (
+<>
+<div className="start_form">
+	<div className="container">
+		<div className="row">
+			<div className="col-lg-10 offset-lg-l">
+				<div className="contact_form_container py-5">
+					<div className="contact_form_title">
+						<h2>Add Election Name</h2>
 					</div>
+					<br />
+
+					<form method="POST" id="contact_form" onSubmit={electionNameForm}>
+
+						<>
+							<div className="form_input">
+								<label>Election name in Bangla</label>
+								<input type="text" onChange={handleInputs} value={electionNameData.banglaelectionname} name="banglaelectionname" id="banglaelectionname" placeholder="Enter your banglaelectionname" />
+							</div>
+							<div className="form_input">
+								<label>Election name in English</label>
+								<input type="text" onChange={handleInputs} value={electionNameData.englishelectionname} name="englishelectionname" id="englishelectionname" placeholder="Enter your englishelectionname" />
+							</div>
+							<div className="form_input">
+								<label>Template Type</label>
+								<input type="text" onChange={handleInputs} value={electionNameData.templatetype} name="templatetype" id="templatetype" placeholder="Enter your templatetype" />
+							</div>
+							<input type="hidden" name="statusfordisplay" defaultValue={statusvalue} />
+							<br />
+							<button type="submit">Submit</button>
+						</>
+				</form>
 				</div>
 			</div>
-
-			<div className="start_form">
-				<div className="container">
-					<div className="row">
-						<div class="result">
-							<h2>Result</h2>
-
-
-							<div>
-								{electionData.map((item) => (
-									<div key={item._id}>
-										<p>Name: {item.banglaelectionname}</p>
-										<p>English name : {item.englishelectionname}</p>
-										<p>template type : {item.templatetype}</p>
-									</div>
-								))}
-							</div>
+		</div>
+	</div>
+</div>
+<div className="start_form">
+	<div className="container">
+		<div className="row">
+			<div class="result">
+				<h2>Result</h2>
+				<div>
+					{electionData.map((item) => (
+						<div key={item._id}>
+							<p>Name: {item.banglaelectionname}</p>
+							<p>English name : {item.englishelectionname}</p>
+							<p>template type : {item.templatetype}</p>
 						</div>
-					</div>
+					))}
 				</div>
 			</div>
-
-		</>
-	)
+		</div>
+	</div>
+</div>
+</>
+)
 }
 
 export default Addelectionname
